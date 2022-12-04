b234351e8de85004a539d54c4facf6913df3b128
akargl <mail@akargl.com>
2022-12-04 12:15:45 +0100

    22-4: refactor a bit

--- 2022/aoc22/src/main/java/com/akargl/Day4.java
+++ 2022/aoc22/src/main/java/com/akargl/Day4.java
@@ -9,29 +9,27 @@ import com.akargl.utils.InputUtils;
 
 public class Day4 {
   public static void main(String[] args) throws IOException {
-    List<String> inputLines = InputUtils.getInputLines(Path.of("inputs/d4_1.txt"), false);
-    long numCompletelyOverlappingSections = Day4.getNumCompletelyOverlappingSections(inputLines);
+    List<List<Integer>> sectionBoundaries = getAllSectionBoundaries(InputUtils.getInputLines(Path.of("inputs/d4_1.txt"), false));
+
+    long numCompletelyOverlappingSections = Day4.getNumCompletelyOverlappingSections(sectionBoundaries);
     System.out.println("Part 1: " + numCompletelyOverlappingSections);
 
-    long numOverlappingSections = Day4.getNumOverlappingSections(inputLines);
+    long numOverlappingSections = Day4.getNumOverlappingSections(sectionBoundaries);
     System.out.println("Part 2: " + numOverlappingSections);
   }
 
-  protected static long getNumCompletelyOverlappingSections(List<String> lines) {
-    return lines.stream().filter(l -> {
-      List<Integer> boundaries = parseSectionBoundaries(l);
-      return (isWithin(boundaries.get(0), boundaries.get(2), boundaries.get(3)) && isWithin(boundaries.get(1), boundaries.get(2), boundaries.get(3)) ||
-          (isWithin(boundaries.get(2), boundaries.get(0), boundaries.get(1)) && isWithin(boundaries.get(3), boundaries.get(0), boundaries.get(1))));
-    }).count();
+  protected static long getNumCompletelyOverlappingSections(List<List<Integer>> sectionBoundaries) {
+    return sectionBoundaries.stream().filter(boundaries -> (isWithin(boundaries.get(0), boundaries.get(2), boundaries.get(3)) && isWithin(boundaries.get(1), boundaries.get(2), boundaries.get(3)) ||
+        (isWithin(boundaries.get(2), boundaries.get(0), boundaries.get(1)) && isWithin(boundaries.get(3), boundaries.get(0), boundaries.get(1))))).count();
   }
 
-  protected static long getNumOverlappingSections(List<String> lines) {
-    return lines.stream().filter(l -> {
-      List<Integer> boundaries = parseSectionBoundaries(l);
+  protected static long getNumOverlappingSections(List<List<Integer>> sectionBoundaries) {
+    return sectionBoundaries.stream().filter(boundaries -> (isWithin(boundaries.get(0), boundaries.get(2), boundaries.get(3)) || isWithin(boundaries.get(1), boundaries.get(2), boundaries.get(3))) ||
+        (isWithin(boundaries.get(2), boundaries.get(0), boundaries.get(1)) || isWithin(boundaries.get(3), boundaries.get(0), boundaries.get(1)))).count();
+  }
 
-      return (isWithin(boundaries.get(0), boundaries.get(2), boundaries.get(3)) || isWithin(boundaries.get(1), boundaries.get(2), boundaries.get(3))) ||
-          (isWithin(boundaries.get(2), boundaries.get(0), boundaries.get(1)) || isWithin(boundaries.get(3), boundaries.get(0), boundaries.get(1)));
-    }).count();
+  protected static List<List<Integer>> getAllSectionBoundaries(List<String> lines) {
+    return lines.stream().map(Day4::parseSectionBoundaries).toList();
   }
 
   protected static List<Integer> parseSectionBoundaries(String line) {

cffd1615bc0e125b6fe714511037d0873a0389c5
akargl <mail@akargl.com>
2024-12-11 23:14:05 +0100

    Day11: p2

--- 2024/aoc24/src/main/java/com/akargl/aoc24/Day11.java
+++ 2024/aoc24/src/main/java/com/akargl/aoc24/Day11.java
@@ -2,45 +2,71 @@ package com.akargl.aoc24;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import com.akargl.aoc24.utils.InputUtils;
 
 public class Day11 {
+  protected static Map<BlinkResult, Long> resultCache = new HashMap<>();
+
   public static void main(String[] args) throws IOException {
     String input = InputUtils.getInputLines("inputs/d11_1.txt").getFirst();
 
-    List<Long> stones = Arrays.stream(input.split(" "))
-        .map(Long::parseLong)
-        .collect(Collectors.toList());
+    List<String> stones = Arrays.stream(input.split(" ")).collect(Collectors.toList());
 
-    long numStonesP1 = part1(stones, 25);
+    long numStonesP1 = findNumStones(stones, 25);
     System.out.println(numStonesP1);
+
+    long numStonesP2 = findNumStones(stones, 75);
+    System.out.println(numStonesP2);
   }
 
-  protected static long part1(List<Long> stones, long numBlinks) {
-    for (int i = 0; i < numBlinks; i++) {
-      System.out.println("Blink " + i+1 + " stones.size " + stones.size());
-      for (int j = 0; j < stones.size(); j++) {
-        Long stone = stones.get(j);
-        if (stone == 0L) {
-          stones.set(j, 1L);
-        } else {
-          String s = String.valueOf(stone);
-          if (s.length() % 2 == 0) {
-            long left = Long.parseLong(s.substring(0, s.length()/2));
-            long right = Long.parseLong(s.substring(s.length()/2));
-            stones.set(j, left);
-            stones.add(j+1, right);
-            j++;
-          } else {
-            stones.set(j, stone * 2024);
-          }
-        }
-      }
+
+  protected static long findNumStones(List<String> stones, long numBlinks) {
+    long numStones = 0;
+
+    for (String stone : stones) {
+      System.out.println("processing initial stone " + stone);
+      numStones += blink(stone, numBlinks);
     }
 
-    return stones.size();
+    return numStones;
   }
 
+  protected static long blink(String stone, long numBlinks) {
+    BlinkResult blinkResult = new BlinkResult(stone, numBlinks);
+    if (resultCache.containsKey(blinkResult)) {
+      return resultCache.get(blinkResult);
+    }
+
+    long numStones;
+
+    if (numBlinks == 0L) {
+      numStones = 1L;
+    } else if (stone.equals("0")) {
+      numStones = blink("1", numBlinks -1);
+    } else if (stone.length() % 2 == 0) {
+      String left = stone.substring(0, stone.length()/2);
+      String right = Arrays.stream(stone.substring(stone.length() / 2)
+          .split(""))
+          .dropWhile(s -> s.equals("0"))
+          .collect(Collectors.joining(""));
+
+      if (right.isEmpty()) {
+        right = "0";
+      }
+
+      numStones = blink(left, numBlinks - 1) + blink(right, numBlinks - 1);
+    } else {
+      long l = Long.parseLong(stone) * 2024L;
+      numStones = blink(String.valueOf(l), numBlinks - 1);
+    }
+
+    resultCache.put(blinkResult, numStones);
+    return numStones;
+  }
+
+  protected record BlinkResult(String stone, long numBlinks) {}
 }
